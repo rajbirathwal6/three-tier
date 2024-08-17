5c8b22e1001d0beba098510f18c31e9e245d7565
rajbirathwal6 <165341945+rajbirathwal6@users.noreply.github.com>
2024-08-16 20:59:19 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+docker run -d --name jenkins \
+  -u root \
+  -p 8080:8080 \
+  -p 50000:50000 \
+  -v jenkins_home:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  jenkins/jenkins:lts
 
 chmod 666 /var/run/docker.sock
 docker build -t frontend .
