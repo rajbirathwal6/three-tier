7a8360d16e683811d4bd0d01d9519c0b3168a2c2
rajbirathwal6 <165341945+rajbirathwal6@users.noreply.github.com>
2024-08-16 20:57:02 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
-whoami
-cd frontend
-sudo chmod 666 /var/run/docker.sock
+
+chmod 666 /var/run/docker.sock
 docker build -t frontend .
-cd . ../backend
+cd ../backend
 docker build -t backend .
 cd ..
 docker-compose up -d
 docker ps
-
